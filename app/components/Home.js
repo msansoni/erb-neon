041000f720d3448bd8362ae43d9a67502388aaef
msansoni <m.h.sansoni@gmail.com>
2020-01-04 00:09:04 +0000

    Import rust neon module

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import hello from '@msansoni/f1-udp-client';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
 
@@ -13,7 +14,7 @@ export default class Home extends Component<Props> {
     return (
       <div className={styles.container} data-tid="container">
         <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
+        <Link to={routes.COUNTER}>{hello.hello()}</Link>
       </div>
     );
   }
